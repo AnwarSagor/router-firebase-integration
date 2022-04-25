6194db4bb88590478b01606405e979f2472943c6
AnwarSagor <sagor.onnet@gmail.com>
2022-04-25 10:18:12 +0600

    submit finished

---
+++ src/App.js
@@ -0,0 +1,30 @@
+
+import { Route, Routes } from 'react-router-dom';
+import './App.css';
+import Header from './components/Header/Header';
+import Home from './components/Home/Home';
+import Login from './components/Login/Login';
+import Orders from './components/Orders/Orders';
+import Products from './components/Products/Products';
+import Register from './components/Register/Register';
+import RequireAuth from './components/RequireAuth/RequireAuth';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello</h1>
+      <Header></Header>
+      <Routes>
+        <Route path="/" element={<Home></Home>}></Route>
+        <Route path='/products' element={<Products></Products>}></Route>
+        <Route path='/orders' element={
+          <RequireAuth><Orders></Orders></RequireAuth>
+        }></Route>
+        <Route path="/register" element={<Register></Register>}></Route>
+        <Route path="/login" element={<Login></Login>}></Route>
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/firebase.init.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyB5UthySaRn4D6Tem-K3xpBOTQANg95hFg",
+    authDomain: "router-firebase-integrat-befdd.firebaseapp.com",
+    projectId: "router-firebase-integrat-befdd",
+    storageBucket: "router-firebase-integrat-befdd.appspot.com",
+    messagingSenderId: "1065285363306",
+    appId: "1:1065285363306:web:81cd9d4e37dfb9ee980c13"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
